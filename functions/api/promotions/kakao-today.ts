a073623c0cb507f0b78881a07120f2371e26cead
wo0h881-dev <wo0h881@gmail.com>
2026-03-23 02:37:22 +0900

    Update kakao-today.ts

--- functions/api/promotions/kakao-today.ts
+++ functions/api/promotions/kakao-today.ts
@@ -1,1 +1,11 @@
+import kakaoData from "../../../public/data/kakao-promotions-today.json";
+
+export const onRequestGet: PagesFunction = async () => {
+  return new Response(JSON.stringify(kakaoData), {
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Cache-Control": "public, max-age=300",
+    },
+  });
+};
 
